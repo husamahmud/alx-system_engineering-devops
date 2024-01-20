2396f63878010e2baa02e956466c687eb84446b1
husamahmud <husamahmud@gmail.com>
2024-01-21 00:30:09 +0300

    Add 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,10 @@
 # using Puppet to make changes to our configuration file
-$content = 'Include /etc/ssh/ssh_config.d/*.conf'
+$content = '
+  Include /etc/ssh/ssh_config.d/*.conf
+  Host *
+  	IdentityFile ~/.ssh/school
+  	PasswordAuthentication no
+'
 file { 'Change-file':
   ensure  => 'file',
   path    => '/etc/ssh/ssh_config',
