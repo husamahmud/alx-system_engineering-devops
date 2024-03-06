c02fd6fe202e09c866b9dc409e202074027968a4
Hüsam <husamahmud@gmail.com>
2024-03-06 03:56:39 +0300

    Add 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module to request data from an Reddit API"""
+"""number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """gets the number of subscribers for a given subreddit"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).json()
+    res = requests.get(url, headers={'User-Agent': 'TutTrue'}).json()
     return res.get('data', {}).get('subscribers', 0)
